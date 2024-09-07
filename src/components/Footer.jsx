3b18180631a258eb54de70defa31af3a8a1262f3
timataliatov <ttaliatov@gmail.com>
2024-09-07 17:04:02 +0200

    We got a logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-base-200 py-8'>
       <div className='container mx-auto px-4 text-center text-base-content'>
-        <p>&copy; {new Date().getFullYear()} Event Manager. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} <span className='font-bold text-accent'>eventify.me</span>. All rights reserved.</p>
       </div>
     </footer>
   );
